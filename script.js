8ca9590f6dd1b8a0e01b955abdd7c1a2b5c8c8bc
rvk2k2 <rahulvk2002@gmail.com>
2024-12-09 18:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask()
+{
+    if(inputbox.value === '')
+    {
+        alert("you must write anythong");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+    }
+    inputbox.value = "";
+    savedata();
+}
+listcontainer.addEventListener("click", (e) =>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+     else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+
+     }
+
+},false);
+
+const savedata = () =>{
+    localStorage.setItem("data",listcontainer.innerHTML);
+
+}
+
+const showlist = () =>{
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showlist();
+
+
